d688d7626cba1836e7d7bfbb7e2c4805b010c548
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-07-27 18:53:29 +0530

    Add files via upload

---
+++ Slicing.py
@@ -0,0 +1,34 @@
+# x='lokesh'
+# print(x[0])
+# print(x[2])
+# print(x[4])
+# print(x[3])
+# print(x[5])
+# print(x[1])
+
+# y='English'
+# print(y[2:])
+# print(y[2:6])
+# print(y[:5])
+# print(y[2:3])
+# print(y[4:1])
+
+# print(y[-4])
+# print(y[-5])
+# print(y[-3])
+# print(y[-2])
+# print(y[-1])
+# print(y[-6])
+# print(y[-7])
+
+# print(y[-4]+y[-2])
+# print(y[0]+y[-4])
+
+"""program to find the sum of the first and last digit"""
+c='542187912245512'
+# print(type(c))
+# print(c[2])
+# print(c[-4])
+# print(c[3]+c[-6])
+# print(int(c[2])+int(c[-2]))
+# print(int(c[-4])+int(c[-6]))
