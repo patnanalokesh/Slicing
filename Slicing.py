2f6d3294896bc9b4d46df2bf25af295ff3d64377
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-07-28 19:06:05 +0530

    Add files via upload

--- Slicing.py
+++ Slicing.py
@@ -24,11 +24,32 @@
 # print(y[-4]+y[-2])
 # print(y[0]+y[-4])
 
-"""program to find the sum of the first and last digit"""
-c='542187912245512'
+# """program to find the sum of the first and last digit"""
+# c='542187912245512'
 # print(type(c))
 # print(c[2])
 # print(c[-4])
 # print(c[3]+c[-6])
 # print(int(c[2])+int(c[-2]))
 # print(int(c[-4])+int(c[-6]))
+
+# f='python'
+# print(f[2:7])
+# print(f[2:-1])
+# print(f[1:-2])
+# print(f[-4:0])
+# print(f[-1:1])
+# print(f[-4:0])
+
+"""Reverse of a string"""
+# n='water'
+# print(n[::-1])
+
+# k='lokesh'
+# print(k[::-1])
+# print(k[::-2])
+# print(k[::-3])
+# print(k[::-4])
+# print(k[::-5])
+# print(k[::-6])
+# print(k[::])
